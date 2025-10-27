3d648b82b2d3e45c28c94e1c46b547549878eb9f
Durim Fetahaj <durim.dev@gmail.com>
2025-10-27 15:17:04 +0100

    fixed render order divs

--- app/page.tsx
+++ app/page.tsx
@@ -10,28 +10,31 @@ export default async function Home() {
         <h1 className="text-2xl font-semibold">Trips</h1>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
+      <div className="flex flex-col gap-6">
         {drivers?.length > 0 ? (
           <>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              {/* <VehicleCreateDialog /> */}
               <div className="bg-white border rounded-2xl p-4 shadow-sm flex flex-col">
                 <p className="text-gray-500 font-medium">Total Drivers</p>
                 <p className="text-2xl font-bold">{drivers.length || 0}</p>
               </div>
-              {/* <div className="bg-white border border-red-500 rounded-2xl p-4 shadow-sm flex flex-col">
+              {/* <VehicleCreateDialog /> */}
+
+              <div className="bg-white border border-red-500 rounded-2xl p-4 shadow-sm flex flex-col">
                 <p className="text-gray-500 font-medium">Drivers with Issues</p>
-              </div> */}
+              </div>
             </div>
 
             {drivers.map((d) => (
-              <DriverCard
-                key={d.id}
-                id={d.id}
-                name={d.fullName}
-                plate={d.vehicles[0]?.licensePlate || "N/A"}
-                hasDifference={d.hasDiscrepancy}
-              />
+              <div key={d.id} className="w-auto max-w-sm">
+                <DriverCard
+                  key={d.id}
+                  id={d.id}
+                  name={d.fullName}
+                  plate={d.vehicles[0]?.licensePlate || "N/A"}
+                  hasDifference={d.hasDiscrepancy}
+                />
+              </div>
             ))}
           </>
         ) : (
