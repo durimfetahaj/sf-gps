1952c8af982e642ac962f6c8f9e17ae9481f9dd7
Durim Fetahaj <durim.dev@gmail.com>
2025-10-27 15:01:17 +0100

    added drivers page title and no trips info

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,38 @@ export default async function Home() {
   const drivers = await getDrivers();
 
   return (
-    <div className="p-6 space-y-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="bg-white border rounded-2xl p-4 shadow-sm flex flex-col">
-          <p className="text-gray-500 font-medium">Total Drivers</p>
-          {<p className="text-2xl font-bold">{drivers.length || 0}</p>}
-        </div>
-        {/* <div className="bg-white border border-red-500 rounded-2xl p-4 shadow-sm flex flex-col">
-          <p className="text-gray-500 font-medium">Drivers with Issues</p>
-        </div> */}
+    <div>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-semibold">Trips</h1>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
-        {drivers.map((d) => (
-          <DriverCard
-            key={d.id}
-            id={d.id}
-            name={d.fullName}
-            plate={d.vehicles[0]?.licensePlate || "N/A"}
-            hasDifference={d.hasDiscrepancy}
-          />
-        ))}
+        {drivers?.length > 0 ? (
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {/* <VehicleCreateDialog /> */}
+              <div className="bg-white border rounded-2xl p-4 shadow-sm flex flex-col">
+                <p className="text-gray-500 font-medium">Total Drivers</p>
+                <p className="text-2xl font-bold">{drivers.length || 0}</p>
+              </div>
+              {/* <div className="bg-white border border-red-500 rounded-2xl p-4 shadow-sm flex flex-col">
+                <p className="text-gray-500 font-medium">Drivers with Issues</p>
+              </div> */}
+            </div>
+
+            {drivers.map((d) => (
+              <DriverCard
+                key={d.id}
+                id={d.id}
+                name={d.fullName}
+                plate={d.vehicles[0]?.licensePlate || "N/A"}
+                hasDifference={d.hasDiscrepancy}
+              />
+            ))}
+          </>
+        ) : (
+          <p>No trips available.</p>
+        )}
       </div>
     </div>
   );
