8b473dd4a97ced694e3b994379cfda4d1debdb30
Durim Fetahaj <durim.dev@gmail.com>
2025-12-02 15:18:11 +0100

    changed main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,16 @@
-import { DriverCard } from "@/components/DriverCards";
-import { getDrivers } from "./actions/drivers";
-import { WorkLogCreateDialog } from "@/components/work-logs/work-log-create-dialog";
-import { getVehicles } from "./actions/vehicle";
+import { PageHeader } from "@/components/page-header";
 
 export default async function Home() {
-  const drivers = await getDrivers();
-  const vehicles = await getVehicles();
-
   return (
-    <div>
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-semibold">Arbeitsprotokolle</h1>
-        {drivers?.length > 0 && <WorkLogCreateDialog vehicles={vehicles} />}
-      </div>
-
-      <div className="flex flex-col gap-6">
-        {drivers?.length > 0 ? (
-          <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="bg-white border rounded-2xl p-4 shadow-sm flex flex-col">
-                <p className="text-gray-500 font-medium">
-                  Gesamtzahl der Fahrer
-                </p>
-                <p className="text-2xl font-bold">{drivers.length || 0}</p>
-              </div>
-
-              <div className="bg-white border border-red-500 rounded-2xl p-4 shadow-sm flex flex-col">
-                <p className="text-gray-500 font-medium">
-                  Fahrer mit Unstimmigkeiten
-                </p>
-                <p className="text-2xl font-bold">
-                  {drivers.filter((d) => d.hasDiscrepancy).length || 0}
-                </p>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:lg:grid-cols-5 gap-5">
-              {vehicles.map((d) => (
-                <div key={d.id} className="w-auto max-w-sm">
-                  <DriverCard
-                    key={d.id}
-                    id={d.driver?.id || "N/A"}
-                    name={d.driver?.fullName || "N/A"}
-                    plate={d.licensePlate}
-                    hasDifference={d.driver?.hasDiscrepancy}
-                  />
-                </div>
-              ))}
-            </div>
-          </>
-        ) : (
-          <p>No trips available.</p>
-        )}
-      </div>
+    <div className="min-h-screen bg-background">
+      <main className="">
+        <div className="">
+          <PageHeader
+            title="Dashboard"
+            description="Overview of your workforce operations"
+          />
+        </div>
+      </main>
     </div>
   );
 }
