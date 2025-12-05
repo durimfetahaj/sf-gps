71a69aeb5ad701421cac4f4a1f244219633c1e12
duro_codes <durim.dev@gmail.com>
2025-12-05 12:44:24 +0100

    login-page

--- app/(auth)/login/components/login-form.tsx
+++ app/(auth)/login/components/login-form.tsx
@@ -1,4 +1,5 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,13 +8,48 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { LoginFormValues, loginSchema } from "@/lib/validators/login";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const router = useRouter();
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: LoginFormValues) {
+    try {
+      /* await createWorker(values); */
+      /* toast.success("Driver created successfully!"); */
+      /*    onSuccess?.(); */
+    } catch (err) {
+      console.error(err);
+      alert("Failed to create vehicle");
+    }
+  }
+
+  const isPending = form.formState.isSubmitting;
+
   return (
     <div
       className={cn("flex flex-col items-center gap-4 px-4 sm:px-0", className)}
@@ -34,32 +70,58 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form aria-label="Login form">
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
+          <Form {...form}>
+            <form
+              aria-label="Login form"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="password"
+                        type="password"
+                        required
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-              <Field>
+              <div className="flex justify-end pt-4">
                 <Button type="submit" className="w-full">
                   Login
                 </Button>
-              </Field>
-            </FieldGroup>
-          </form>
+              </div>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
